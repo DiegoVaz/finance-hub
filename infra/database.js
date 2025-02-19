868f8837cfcaa74a3ceb06d00d6c27b3fecb6c40
Diego Guimarães Vaz <diegovaz.ti@gmail.com>
2025-02-19 20:46:00 +0000

    modify `database`

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,16 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   })
 
-  await client.connect()
-  const result = await client.query(queryObject)
-  client.end()
+  try {
+    await client.connect()
+    const result = await client.query(queryObject)
 
-  return result
+    return result
+  } catch (err) {
+    console.error(err)
+  } finally {
+    await client.end()
+  }
 }
 
 export default {
