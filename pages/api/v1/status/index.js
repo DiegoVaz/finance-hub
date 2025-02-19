a61f85390183f2adf3eb156cd1dcb45369d7063f
Diego Guimarães Vaz <diegovaz.ti@gmail.com>
2025-02-19 20:47:13 +0000

    add rota `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
-  response.status(200).json({ status: 'Online' })
+  const databaseName = process.env.POSTGRES_DB
+  const databaseVersion = await database.query('SHOW server_version')
+  const databaseMaxConections = await database.query('SHOW max_connections')
+  const databaseOpenedConnections = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity where datname = $1 ;`,
+    values: [databaseName],
+  })
+
+  const updateAt = new Date().toISOString()
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConections.rows[0].max_connections
+        ),
+        opened_connections: databaseOpenedConnections.rows[0].count,
+      },
+    },
+  })
 }
 
 export default status
