bb307d564ba74818faf0aa7f0936819ad32ec8d6
Diego Vaz <diegovaz.ti@gmail.com>
2025-02-26 05:22:00 -0300

    update `compose.yaml`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -8,7 +8,7 @@ test('GET to /api/v1/status should return 200', async () => {
   const praseUpdateAt = new Date(responseBody.update_at).toISOString()
   expect(responseBody.update_at).toEqual(praseUpdateAt)
 
-  expect(responseBody.dependencies.database.version).toEqual('16.7')
+  expect(responseBody.dependencies.database.version).toEqual('16.8')
   expect(responseBody.dependencies.database.max_connections).toEqual(100)
   expect(responseBody.dependencies.database.opened_connections).toEqual(1)
 })
