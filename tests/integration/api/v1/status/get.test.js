d2eb7bbeffc2363679d759e06688139c2c55d6e9
Diego Vaz <diegovaz.ti@gmail.com>
2025-03-19 11:16:50 -0300

    fix: fix test get status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,10 +11,10 @@ describe('GET /api/v1/status', () => {
       expect(response.status).toBe(200)
 
       const responseBody = await response.json()
-      expect(responseBody.update_at).toBeDefined()
+      expect(responseBody.updated_at).toBeDefined()
 
       const praseUpdateAt = new Date(responseBody.update_at).toISOString()
-      expect(responseBody.update_at).toEqual(praseUpdateAt)
+      expect(responseBody.updated_at).toEqual(praseUpdateAt)
 
       expect(responseBody.dependencies.database.version).toEqual('17.4')
       expect(responseBody.dependencies.database.max_connections).toEqual(100)
